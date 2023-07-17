4f555680ba20d0f40f38bc179ca6b1401b9e09a1
PhamThao1206 <132880186+PhamThao1206@users.noreply.github.com>
2023-07-17 20:29:59 +0700

    Create pdo.php

---
+++ category/pdo.php
@@ -0,0 +1,69 @@
+<?php
+
+abstract class Connection {
+    protected $host;
+    protected $db;
+    protected $username;
+    protected $pass;
+    protected $connection;
+
+    public function __construct()
+    {
+        $this->host = 'localhost';
+        $this->db = 'crud_test';
+        $this->username = 'root';
+        $this->pass = '';
+        $this->connection = $this->connect();
+        
+    }
+    public function connect(){
+        try {
+            $conn = new PDO("mysql:host=$this->host;dbname=$this->db",$this->username, $this->pass);
+            $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+            return $conn;
+        }catch (Expection $e) {
+            die($e->getMessage());
+        }
+    }
+    public function prepareSQL($sql){
+        return $this->connection->prepare($sql);
+    }
+    
+}
+class CategoryConnection extends Connection{
+    public function getData(){
+        $sql = "SELECT * FROM category";
+        $select = $this->prepareSQL($sql);
+        $select->execute();
+        return $select->fetchAll();
+    }
+    
+    public function getOneData($data){
+        $sql = "SELECT * FROM category WHERE id = :id";
+        $select = $this->prepareSQL($sql);
+        $select->execute($data);
+        return $select->fetchAll();
+    }
+    
+    public function createNewData($data){
+        $sql = "INSERT INTO category (name) VALUES (:name)";
+        $create = $this->prepareSQL($sql);
+        $create->execute($data);
+    }
+    
+    public function updateData($data){
+        $sql = "UPDATE category SET name = :name WHERE id = :id";
+        $update = $this->prepareSQL($sql);
+        $update->execute($data);
+    }
+    public function deleteData($data){
+        $sql = "DELETE FROM category WHERE id = :id";
+        $update = $this->prepareSQL($sql);
+        $update->execute($data);
+    }
+
+}
+
+
+    
+?>
